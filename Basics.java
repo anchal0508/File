24c34da6bb58382de134853f3c32fbe8beed933e
ABCROB TECHNOLOGIES <57563093+anchal0508@users.noreply.github.com>
2025-08-22 15:13:29 +0000

    new

--- Basics.java
+++ Basics.java
@@ -1,5 +1,8 @@
-class Basics{
-    public static void main(String [] args){
-        System.out.println("Hello");
+class Basics {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("hello " + i);
+        }
+        System.out.println("Done");
     }
 }
