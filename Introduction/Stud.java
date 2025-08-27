87d25970ba207f5b2e522e8b95abaf50633b5151
ABCROB TECHNOLOGIES <57563093+anchal0508@users.noreply.github.com>
2025-08-27 16:20:40 +0000

    class 1

---
+++ Introduction/Stud.java
@@ -0,0 +1,25 @@
+/*
+class Stud{
+    public static void main(String[] args) {
+        int a = 3;
+        int b  = 2;
+        int c = a+b; // c = 5
+        c = c+1; // 6
+        c+=1;   // 7
+        c++;    // 8 (Post increment Operator)
+        ++c;    // 9 (Pree increment Operator)
+        System.out.println(c);
+    }
+}
+    */
+
+
+    class Stud{
+    public static void main(String[] args) {
+        int a = 3;
+        int b  = 2;
+        int c = a+b; // c = 5
+        System.out.println(c++); // 5
+        System.out.println(c++);
+    }
+}
