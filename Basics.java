8323f0ce77a506725c83f756981790d52271ec05
ABCROB TECHNOLOGIES <57563093+anchal0508@users.noreply.github.com>
2025-08-28 20:24:10 +0530

    Delete Basics.java

--- Basics.java
+++
@@ -1,8 +0,0 @@
-class Basics {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("hello " + i);
-        }
-        System.out.println("Done");
-    }
-}
