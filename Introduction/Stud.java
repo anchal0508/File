4b2876822f4f5baf48b3a69759a74229c9056753
ABCROB TECHNOLOGIES <57563093+anchal0508@users.noreply.github.com>
2025-08-29 22:17:25 +0530

    Update Stud.java

--- Introduction/Stud.java
+++ Introduction/Stud.java
@@ -53,6 +53,7 @@ class Stud {
 }
 
  */
+/*
 //Swapping: Without Using Third variable
 class Stud {
 	public static void main(String[] Spider) {
@@ -68,3 +69,104 @@ class Stud {
 
 	}
 }
+
+*/
+/*
+
+// Command Line Argument
+class Stud {
+	public static void main(String[] userVar) {
+		if(userVar.length>0)
+			for(int i=0;i<userVar.length;i++)
+				System.out.println(userVar[i]);
+
+	}
+}
+*/
+/*
+// Power
+class Stud {
+	public static void main(String[] args) {
+		int p = 1;
+		for (int i = 0; i < 2; i++) {
+			p = p*3;
+		}
+		System.out.println(p);
+	}
+}
+
+ */
+/*
+// Power Using Loop
+ class Stud {
+	public static void main(String[] args) {
+		int p = 1;
+		for (int i = 0; i < 2; i++) {
+			p = p*3;
+		}
+		System.out.println(p);
+	}
+}
+
+ */
+/*
+ // Find Gretest Number
+class Stud {
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 7;
+		int c = 7;
+
+		if (a >= b && a >= c) {
+			System.out.println(a);
+		}
+		else if (b >= a && b >= c) {
+			System.out.println(b);
+		}
+		else if (c >= a && c >= b) {
+			System.out.println(c);
+		}
+	}
+}
+	
+*/
+
+/*
+// Divisible By 5 and 11
+class Stud {
+	public static void main(String[] args) {
+		int num = 550;
+		if (num % 5 == 0 && num % 11 == 0) {
+			System.out.println("Divisible BY 5 and 11");
+		} else {
+			System.out.println("Not Divisible");
+		}
+	}
+}
+
+ */
+/*
+// Leap 
+class Stud {
+	public static void main(String[] args) {
+		int year = 1900;
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			System.out.println(year + " : is a Leap Year");
+		} else {
+			System.out.println(year + ": is Not a Leap Year");
+		}
+	}
+}*/
+/*
+// Alphabate  testing 
+class Stud {
+	public static void main(String[] args) {
+		char var = 'T';
+		if (var >= 'a' && var <= 'z')
+			System.out.println("Lower case");
+		if (var >= 'A' && var <= 'Z')
+			System.out.println("Upper case");
+	}
+}
+
+*/
