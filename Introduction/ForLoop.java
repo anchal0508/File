d67faf6bf640939e6024e03505f79c4c85afa120
ABCROB TECHNOLOGIES <57563093+anchal0508@users.noreply.github.com>
2025-09-01 22:17:01 +0530

    Update ForLoop.java

--- Introduction/ForLoop.java
+++ Introduction/ForLoop.java
@@ -54,6 +54,7 @@ class ForLoop {
  
  
  */
+/*
 class ForLoop {
 	public static void main(String[] args) {
 		for (int j = 1; j <= 10; j++) {
@@ -63,5 +64,86 @@ class ForLoop {
 			System.out.println();
 		}
 
+	}
+}
+
+*/
+
+
+
+/*
+class Stud {
+	public static void main(String[] args) {
+
+		for (int j = 0; j <= 9; j++) {
+			
+			for (int i = 9; i >= j; i--) {
+				System.out.print("  ");
+			}
+			for (int i = 0; i <= j; i++) {
+				System.out.print(i+" ");
+			}
+			for (int i = 1; i <= j; i++) {
+				System.out.print(i+" ");
+			}
+			 
+			System.out.println();
+		}
+
+
+
+		for (int j = 8; j >= 0; j--) {
+			
+			for (int i = 9; i >= j; i--) {
+				System.out.print("  ");
+			}
+			for (int i = 0; i <= j; i++) {
+				System.out.print(i+" ");
+			}
+			for (int i = 1; i <= j; i++) {
+				System.out.print(i+" ");
+			}
+			 
+			System.out.println();
+		}
+
+	}
+}
+
+*/
+
+class Stud {
+	public static void main(String[] args) {
+
+		for (int j = 9; j >= 4; j--) {
+
+			for (int i = 9; i >= j; i--) {
+				System.out.print("  ");
+			}
+			for (int i = 0; i <= j; i++) {
+				System.out.print(i + " ");
+			}
+			for (int i = j - 1; i >= 0; i--) {
+				System.out.print(i + " ");
+			}
+
+			System.out.println();
+		}
+		for (int j = 4; j <= 9; j++) {
+
+			for (int i = 9; i >= j; i--) {
+				System.out.print("  ");
+			}
+			for (int i = 0; i <= j; i++) {
+				System.out.print(i + " ");
+			}
+			for (int i = j - 1; i >= 0; i--) {
+				System.out.print(i + " ");
+			}
+
+			System.out.println();
+		}
+
+		
 	}
 }
